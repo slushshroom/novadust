333175f06233fd4ab8a9eec6cfe4ea05e4e2c407
slushshroom <werrellh@gmail.com>
2025-09-02 15:22:22 -0600

    Update alacritty.nix

--- home-manager/modules/themes/novadust/alacritty.nix
+++ home-manager/modules/themes/novadust/alacritty.nix
@@ -1,4 +1,4 @@
-## alacritty.nix - Terminal theming module
+## alacritty.nix - Terminal theming module## alacritty.nix - Terminal theming module
 { config, pkgs, lib, ... }:
 
 let
@@ -72,10 +72,10 @@ in
               foreground = colors.bg.primary;
               background = colors.warm.yellow;
             };
-            focused_match = {
-              foreground = colors.bg.primary;
-              background = colors.warm.orange;
-            };
+          focused_match = {
+            foreground = "0d1421";  # bg.primary
+            background = "ffa726";  # warm.orange
+          };
           };
           
           line_indicator = {
@@ -85,47 +85,47 @@ in
           
           selection = {
             text = "CellForeground";
-            background = colors.ui.selected;
+            background = "264f78";  # ui.selected
           };
           
           normal = {
-            black = colors.bg.primary;
-            red = colors.ui.error;
-            green = colors.cool.green;
-            yellow = colors.warm.orange;
-            blue = colors.blue.primary;
-            magenta = colors.cool.purple;
-            cyan = colors.cool.cyan;
-            white = colors.text.primary;
+            black = "0d1421";   # bg.primary
+            red = "f85149";     # ui.error
+            green = "66bb6a";   # cool.green
+            yellow = "ffa726";  # warm.orange
+            blue = "4a9eff";    # blue.primary
+            magenta = "ab47bc"; # cool.purple
+            cyan = "4dd0e1";    # cool.cyan
+            white = "e6edf3";   # text.primary
           };
           
           bright = {
-            black = colors.text.muted;
-            red = "#ff8a80";
-            green = "#a5d6a7";
-            yellow = "#ffcc80";
-            blue = colors.blue.light;
-            magenta = "#ce93d8";
-            cyan = "#80deea";
-            white = colors.text.primary;
+            black = "7d8590";   # text.muted
+            red = "ff8a80";
+            green = "a5d6a7";
+            yellow = "ffcc80";
+            blue = "6bb6ff";    # blue.light
+            magenta = "ce93d8";
+            cyan = "80deea";
+            white = "e6edf3";   # text.primary
           };
           
           dim = {
-            black = colors.bg.secondary;
-            red = "#c62828";
-            green = "#2e7d32";
-            yellow = "#f57c00";
-            blue = colors.blue.dark;
-            magenta = "#7b1fa2";
-            cyan = "#00838f";
-            white = colors.text.secondary;
+            black = "1a2332";   # bg.secondary
+            red = "c62828";
+            green = "2e7d32";
+            yellow = "f57c00";
+            blue = "2968a3";    # blue.dark
+            magenta = "7b1fa2";
+            cyan = "00838f";
+            white = "b6c2cf";   # text.secondary
           };
         };
         
         bell = {
           animation = "EaseOutExpo";
           duration = 0;
-          color = colors.ui.warning;
+          color = "d29922";  # ui.warning
         };
         
         mouse = {
@@ -153,4 +153,5 @@ in
       };
     };
   };
+}
 }
